c5dea4e9173d0b3f9c3bd99dc41051d27421ad09
Giorgio Chiesa <giorgio.chiesa@unito.it>
2025-11-29 11:33:18 +0100

    UpdateFile

--- upload.sh
+++ upload.sh
@@ -1,5 +1,33 @@
+usage() { echo "use -m Message  or default is -m UpdateFile"; exit 0;}
+
+while getopts "hm:" o; do
+    case ${o} in
+        h)
+            echo "Help Message";
+            usage
+            ;;
+        m)
+            m=${OPTARG};
+            echo "Message = ${m}"
+            ;;
+        *)
+            m="UpdateFile";
+            echo "Default value: UpdateFile"
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${m}" ]; then
+    m="UpdateFile";
+fi
+
+echo "Message = ${m}"
+
+
+
 
 git fetch
 git add .
-git commit -m "Update files"
+git commit -m "${m}"
 git push 
